46450b50554b2529664ed2e9583c07928cd65e5a
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2024-01-13 22:39:02 +0800

    Change createElement to <template>
    Change code to use an HTML `<template>` instead of
    `document.createElement()`

--- js/main.js
+++ js/main.js
@@ -1,71 +1,83 @@
 window.addEventListener('load', () => {
-    const lists_container = document.querySelector('section.lists')
+    const list_container = (document.querySelector('section.lists'))
+    const template = document.querySelector('.list-block-template')
+
+    if (! template) {
+        console.error('An unexpected error occurred. The list block template could not be found.')
+        return false
+    }
 
     // Process each list and its items from the URL query string
     const params = new URLSearchParams(window.location.search)
-    for (const param of params.keys()) {
-        const list_name = param.match(/l(\d+)name/)
-        if (list_name !== null) {
-            const list_number = list_name[1]
 
-            const list_block = document.createElement('div')
+    const query_parameters = Array.from(params.keys())
+    const list_names = query_parameters
+        .map((param) => {
+            const list_name_match = param.match(/l(\d+)name/)
+            if (list_name_match === null) return null
+            return {
+                name: params.get(list_name_match[0]) ?? '',
+                number: list_name_match[1] ?? ''
+            }
+        })
+        .filter((list) => list !== null)
 
-            const list_heading = document.createElement('h2')
-            list_heading.appendChild( document.createTextNode(params.get(list_name[0])) )
+    list_names.forEach((list_name, list_index) => {
+        const list_number = list_name.number
 
-            list_block.appendChild(list_heading)
+        const tc = template.content
 
-            // Create an array from provided list items (empty if not provided)
-            const list_items = params.get(`l${list_number}items`)
-            const list_items_array = (function () {
-                if (list_items !== null) return list_items.split(',')
-                return []
-            })()
+        const list_block = tc.querySelector('div.list').cloneNode()
+        list_block.setAttribute('id', 'l' + list_index)
+        list_container.appendChild(list_block)
 
-            // Create an array from provided list item checkbox values (empty if not provided)
-            const list_checks = params.get(`l${list_number}checks`)
-            const list_checks_array = (function () {
-                if (list_checks !== null) return list_checks.split('')
-                return Array(list_items_array.length).fill(0)
-            })()
+        const list_heading = tc.querySelector('.list__heading').cloneNode()
+        list_block.appendChild(list_heading)
 
-            if (list_items_array.length > 0) {
-                const list = document.createElement('ul')
-                const items = list_items_array.map((item_text, index) => {
-                    const item = document.createElement('li')
+        const list_ul = tc.querySelector('.list ul').cloneNode()
 
-                    const checkbox = document.createElement('input')
-                    checkbox.setAttribute('type', 'checkbox')
-                    checkbox.setAttribute('name', `l${list_number}-${index}`)
-                    checkbox.setAttribute('id', `l${list_number}-${index}`)
-                    checkbox.setAttribute('value', item_text)
-                    
-                    if (list_checks_array[index] === '1') {
-                        checkbox.setAttribute('checked', true)
+        const list_items = params.get(`l${list_number}items`)
+        if (list_items !== null) {
+            const item_texts = list_items.split(',')
+            const items = item_texts.map((item_text, item_index) => {
+                const item_id = `l${list_index}-${item_index}`
+
+                const li = tc.querySelector('.list__item').cloneNode(true)
+
+                const checkbox = li.querySelector('input[type=checkbox]')
+                checkbox.setAttribute('id', item_id)
+                checkbox.removeAttribute('checked')
+
+                const label = li.querySelector('label')
+                label.setAttribute('for', item_id)
+                label.innerText = item_text
+
+                return li
+            })
+
+            const checks = params.get(`l${list_number}checks`)
+            if (checks !== null) {
+                const checks_array = checks.split('')
+                items.forEach((item, index) => {
+                    const check_value = checks_array[index] ?? '0'
+                    const item_checkbox = item.querySelector('input[type=checkbox]')
+                    if (check_value === '1') {
+                        item_checkbox.setAttribute('checked', 'checked')
+                    } else {
+                        item_checkbox.removeAttribute('checked')
                     }
-
-                    const label = document.createElement('label')
-                    label.appendChild(checkbox)
-                    label.appendChild( document.createTextNode(item_text) )
-
-                    item.appendChild(label)
-
-                    return item
                 })
-
-                for (const item of items) {
-                    list.appendChild(item)
-                }
-
-                list_block.appendChild(list)
-            } else {
-                const none_message = document.createElement('p')
-                none_message.appendChild( document.createTextNode('No items') )
-
-                list_block.appendChild(none_message)
             }
 
-            lists_container.appendChild(list_block)
+            for (const item of items) {
+                list_ul.appendChild(item)
+            }
+            list_block.appendChild(list_ul)
+        } else {
+            const no_items_message = document.createElement('p')
+            no_items_message.appendChild( document.createTextNode('No items') )
+
+            list_block.appendChild(no_items_message)
         }
-    }
+    })
 })

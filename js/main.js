35895da2dbc485f534f0a8a0f50df542ec60bcb6
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2024-01-15 23:26:40 +0800

    Implement "Add Item" functionality

--- js/main.js
+++ js/main.js
@@ -28,7 +28,7 @@ window.addEventListener('load', () => {
         const tc = template.content
 
         const list_block = tc.querySelector('div.list').cloneNode()
-        list_block.setAttribute('id', 'l' + list_index)
+        list_block.setAttribute('id', 'l' + list_number)
         list_container.appendChild(list_block)
 
         const list_heading = tc.querySelector('.list__heading').cloneNode()
@@ -36,6 +36,8 @@ window.addEventListener('load', () => {
         list_block.appendChild(list_heading)
 
         const list_ul = tc.querySelector('.list ul').cloneNode()
+
+        list_block.appendChild(tc.querySelector('.list__add-item').cloneNode(true))
 
         const list_items = params.get(`l${list_number}items`)
         if (list_items !== null) {
@@ -74,11 +76,37 @@ window.addEventListener('load', () => {
                 list_ul.appendChild(item)
             }
             list_block.appendChild(list_ul)
+
+            list_block.appendChild(tc.querySelector('.list__add-item').cloneNode(true))
         } else {
             const no_items_message = document.createElement('p')
             no_items_message.appendChild( document.createTextNode('No items') )
 
             list_block.appendChild(no_items_message)
         }
+    
+        const add_item_inputs = list_block.querySelectorAll('.list__add-item input')
+        add_item_inputs.forEach(input => input.addEventListener('change', addItem))
     })
 })
+
+function addItem(event) {
+    const input = event.target
+    const params = new URLSearchParams(window.location.search)
+    const list_block = input.closest('.list')
+    const list_id = list_block.getAttribute('id')
+
+    const list_items = params.get(`${list_id}items`)
+    const list_items_array = list_items && list_items.split(',') || []
+    list_items_array.push(input.value)
+    params.set(`${list_id}items`, list_items_array)
+
+    const list_checks = params.get(`${list_id}checks`) ?? ''
+
+    params.set(`${list_id}checks`, list_checks + '0')
+
+    // The toString method of URLSearchParams encodes commas to '%2C', but we
+    // want the URL to remain user-friendly and hackable, so we change it back
+    // to commas
+    window.location.href = '?' + params.toString().replace(/%2C/g, ',')
+}

66dbdd1f3a7ba0019b99bc4e0dc22ceaeda38b4e
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2024-02-04 06:20:22 +0800

    feat: add handling of ordered lists
    Add handling of ordered lists specified in the query string using a list
    type parameter. e.g. `l2type=ol`.
    
    Also:
    - Add an unordered list to the _sample list_ link.
    - Tweak template to support drag & drop (coming soon).
    - Remove some excessive error handling. The template and the JS code
      that handles it need to match.
    - Refactor:
        - Process all the list properties in the same place and improve the
          `lists` object.
        - Eliminate some redundant variables.
    - TODO comments:
        - We need handling of checkbox changes
        - We need a way to select a list type (ordered/unordered) for new
          lists and change the type for existing lists.

--- js/main.js
+++ js/main.js
@@ -1,59 +1,58 @@
 window.addEventListener('load', () => {
     const lists_container = (document.querySelector('section.lists'))
-    const template = document.querySelector('.list-block-template')
-
-    if (! template) {
-        console.error('An unexpected error occurred. The list block template could not be found.')
-        return false
-    }
-
+    const template = document.querySelector('.list-template')
     const tc = template.content
 
     lists_container.appendChild(tc.querySelector('.add-list').cloneNode(true))
 
     // Process each list and its items from the URL query string
     const params = new URLSearchParams(window.location.search)
-    const list_names = getListNames(params)
+    const lists = getLists(params)
 
-    list_names.forEach((list_name) => {
-        const list_number = list_name.number
-
+    lists.forEach((list) => {
         const list_block = tc.querySelector('.list').cloneNode()
-        list_block.setAttribute('id', 'l' + list_number)
+        list_block.setAttribute('id', 'l' + list.number)
         lists_container.appendChild(list_block)
 
         const list_heading = tc.querySelector('.list__heading').cloneNode()
-        list_heading.innerText = list_name.name
+        list_heading.appendChild(document.createTextNode(list.name))
         list_block.appendChild(list_heading)
 
-        const list_ul = tc.querySelector('.list ul').cloneNode()
+        // TODO: Add a list type selector (ordered / unordered)
 
-        const list_items = params.get(`l${list_number}items`)
-        if (list_items !== null) {
+        const list_list = tc.querySelector(`.list ${list.type}`).cloneNode()
+
+        if (list.items !== null) {
             list_block.appendChild(tc.querySelector('.list__add-item').cloneNode(true))
 
-            const item_texts = list_items.split(',')
-            const items = item_texts.map((item_text, item_index) => {
-                const item_id = `l${list_number}-${item_index}`
+            const item_texts = list.items.split(',')
+            const items = item_texts.map((text, index) => {
+                const item_number = index + 1
+                const item_id = `l${list.number}-${item_number}`
 
-                const li = tc.querySelector('.list__item').cloneNode(true)
+                const li = tc.querySelector('li').cloneNode(true)
 
                 const checkbox = li.querySelector('input[type=checkbox]')
                 checkbox.setAttribute('id', item_id)
+                checkbox.setAttribute('name', item_id)
                 checkbox.removeAttribute('checked')
 
+                // TODO: Add a handler for checkbox change event
+
+                // Since we did a _deep_ clone of the list item, we replace the
+                // `innerText` of the label rather than just appending a
+                // `document.createTextNode`
                 const label = li.querySelector('label')
                 label.setAttribute('for', item_id)
-                label.innerText = item_text
+                label.innerText = text
 
                 return li
             })
 
-            const checks = params.get(`l${list_number}checks`)
-            if (checks !== null) {
-                const checks_array = checks.split('')
+            if (list.checks !== null) {
+                const checks = list.checks.split('')
                 items.forEach((item, index) => {
-                    const check_value = checks_array[index] ?? '0'
+                    const check_value = checks[index] ?? '0'
                     const item_checkbox = item.querySelector('input[type=checkbox]')
                     if (check_value === '1') {
                         item_checkbox.setAttribute('checked', 'checked')
@@ -64,14 +63,13 @@ window.addEventListener('load', () => {
             }
 
             for (const item of items) {
-                list_ul.appendChild(item)
+                list_list.appendChild(item)
             }
-            list_block.appendChild(list_ul)
+            list_block.appendChild(list_list)
         } else {
-            const no_items_message = document.createElement('p')
-            no_items_message.appendChild( document.createTextNode('No items') )
-
-            list_block.appendChild(no_items_message)
+            const no_items = document.createElement('p')
+            no_items.appendChild( document.createTextNode('No items') )
+            list_block.appendChild(no_items)
         }
 
         list_block.appendChild(tc.querySelector('.list__add-item').cloneNode(true))
@@ -80,7 +78,7 @@ window.addEventListener('load', () => {
         add_item_inputs.forEach(input => input.addEventListener('change', addItem))
     })
 
-    if (list_names.length > 0) {
+    if (lists.length > 0) {
         lists_container.appendChild(tc.querySelector('.add-list').cloneNode(true))
     }
 
@@ -101,11 +99,15 @@ function addItem(event) {
     list_items_array.push(input.value)
     params.set(`${list_id}items`, list_items_array)
 
-    const list_checks = params.get(`${list_id}checks`) ?? ''
-    params.set(`${list_id}checks`, list_checks + '0')
+    // Recreate the checks parameter value based on the actual checkbox values
+    const checks = Array.from(list_block.querySelectorAll('input[type=checkbox]'))
+        .map(checkbox => checkbox.checked ? '1' : '0')
+        .join('')
 
-    const tc = document.querySelector('.list-block-template').content
-    const new_item = tc.querySelector('.list__item').cloneNode(true)
+    params.set(`${list_id}checks`, checks + '0')
+
+    const tc = document.querySelector('.list-template').content
+    const new_item = tc.querySelector('.list li').cloneNode(true)
     const new_item_number = list_items_array.length - 1
     const new_item_id = `${list_id}-${new_item_number}`
 
@@ -115,11 +117,13 @@ function addItem(event) {
     new_item_input.setAttribute('id', new_item_id)
     new_item_input.removeAttribute('checked')
 
+    // Since we _deep_ cloned the template list item, we replace the `innerText`
+    // rather than just appending `document.createTextNode`
     const new_item_label = new_item.querySelector('label')
     new_item_label.setAttribute('for', new_item_id)
     new_item_label.innerText = input.value
 
-    list_block.querySelector('ul').appendChild(new_item)
+    list_block.querySelector('ul, ol').appendChild(new_item)
 
     // The toString method of URLSearchParams encodes commas to '%2C', but we
     // want the URL to remain user-friendly and hackable, so we change it back
@@ -134,15 +138,18 @@ function addList(event) {
     const lists_container = input.closest('.lists')
     const params = new URLSearchParams(window.location.search)
 
-    const list_names = getListNames(params)
+    const list_names = getLists(params)
 
-    const tc = document.querySelector('.list-block-template').content
+    const tc = document.querySelector('.list-template').content
     const new_list = tc.querySelector('.list').cloneNode()
     const new_list_heading = tc.querySelector('.list__heading').cloneNode()
     const new_add_item = tc.querySelector('.list__add-item').cloneNode(true)
 
     new_list_heading.innerText = input.value
     new_list.appendChild(new_list_heading)
+
+    // TODO: Add a list type selector (ordered / unordered)
+
     new_list.appendChild(new_add_item)
     lists_container.appendChild(new_list)
 
@@ -165,14 +172,21 @@ function addList(event) {
     input.value = ''
 }
 
-function getListNames(params) {
+function getLists(params) {
     return Array.from(params.keys())
         .map((param) => {
             const list_name_match = param.match(/l(\d+)name/)
+
             if (list_name_match === null) return null
+
+            const number = list_name_match[1]
+
             return {
                 name: params.get(list_name_match[0]),
-                number: list_name_match[1],
+                number: number,
+                type: params.get(`l${number}type`),
+                items: params.get(`l${number}items`),
+                checks: params.get(`l${number}checks`),
             }
         })
         .filter((list) => list !== null)

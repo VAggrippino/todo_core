00fbfdf4f3ad2c4d625cdb208b19c97e94a1d898
Vince Aggrippino <195491+VAggrippino@users.noreply.github.com>
2024-02-07 06:23:00 +0800

    fix: Toggle URL checks value when changing checkbox
    Resolves #1
    
    /js/main.js :
    - Add a toggleCheck event handler for checkboxes' `change` event that
      updates the `l<list_id>checks` query parameter.
    - Move the code that updates the URL to a setUrl function rather than
      repeating it.
    - Move code to functions to reduce repetition.
    - Handle commas in list items.
    - Use `scrollIntoView` to smoothly scroll to new list instead of using
      the fragment identifier, then focus the new list's _Add Item_ field.
    
      /index.html :
      - Add a link to start over with no lists

--- js/main.js
+++ js/main.js
@@ -3,211 +3,216 @@ window.addEventListener('load', () => {
     const template = document.querySelector('.list-template')
     const tc = template.content
 
-    lists_container.appendChild(tc.querySelector('.add-list').cloneNode(true))
-
     // Process each list and its items from the URL query string
-    const params = new URLSearchParams(window.location.search)
-    const lists = getLists(params)
+    const lists = getLists()
 
-    lists.forEach((list) => {
-        const list_block = tc.querySelector('.list').cloneNode()
-        list_block.setAttribute('id', 'l' + list.number)
-        lists_container.appendChild(list_block)
-
-        const list_heading = tc.querySelector('.list__heading').cloneNode()
-        list_heading.appendChild(document.createTextNode(list.name))
-        list_block.appendChild(list_heading)
-
-        // TODO: Add a list type selector (ordered / unordered)
-
-        const list_list = tc.querySelector(`.list ${list.type}`).cloneNode()
-
-        if (list.items !== null) {
-            list_block.appendChild(tc.querySelector('.list__add-item').cloneNode(true))
-
-            const item_texts = list.items.split(',')
-            const items = item_texts.map((text, index) => {
-                const item_number = index + 1
-                const item_id = `l${list.number}-${item_number}`
-
-                const li = tc.querySelector('li').cloneNode(true)
-
-                const checkbox = li.querySelector('input[type=checkbox]')
-                checkbox.setAttribute('id', item_id)
-                checkbox.setAttribute('name', item_id)
-                checkbox.removeAttribute('checked')
-
-                // TODO: Add a handler for checkbox change event
-
-                // Since we did a _deep_ clone of the list item, we replace the
-                // `innerText` of the label rather than just appending a
-                // `document.createTextNode`
-                const label = li.querySelector('label')
-                label.setAttribute('for', item_id)
-                label.innerText = text
-
-                return li
-            })
-
-            if (list.checks !== null) {
-                const checks = list.checks.split('')
-                items.forEach((item, index) => {
-                    const check_value = checks[index] ?? '0'
-                    const item_checkbox = item.querySelector('input[type=checkbox]')
-                    if (check_value === '1') {
-                        item_checkbox.setAttribute('checked', 'checked')
-                    } else {
-                        item_checkbox.removeAttribute('checked')
-                    }
-                })
-            }
-
-            for (const item of items) {
-                list_list.appendChild(item)
-            }
-            list_block.appendChild(list_list)
-        } else {
-            const no_items = document.createElement('p')
-            no_items.appendChild( document.createTextNode('No items') )
-            list_block.appendChild(no_items)
-        }
-
-        list_block.appendChild(tc.querySelector('.list__add-item').cloneNode(true))
-    
-        const add_item_inputs = list_block.querySelectorAll('.list__add-item input')
-        add_item_inputs.forEach(input => input.addEventListener('change', addItem))
-    })
-
-    if (lists.length > 0) {
-        lists_container.appendChild(tc.querySelector('.add-list').cloneNode(true))
+    if (lists.length === 0) {
+        const no_lists = document.createElement('p')
+        no_lists.classList.add('no-lists')
+        no_lists.appendChild(document.createTextNode('No lists.'))
+        lists_container.appendChild(no_lists)
     }
 
-    lists_container.querySelectorAll('.add-list input').forEach((input) => {
-        input.addEventListener('change', addList)
+    // An initial _New List_ field should be after a "No lists" message, but
+    // before any lists
+    const add_new_list_block = addNewListField(lists_container)
+
+    // Build each list from the query parameters' data
+    lists.forEach((list) => {
+        const list_block = addList({
+            container: lists_container,
+            heading: list.name,
+        })
+
+        if (list.items !== null) {
+            addItems({
+                values: list.items,
+                checks: list.checks ?? '',
+                list_block
+            })
+        } else {
+            addNoItemsMessage(list_block)
+        }
     })
 })
 
-function addItem(event) {
-    const input = event.target
+function addItems({values, checks, list_block}) {
     const params = new URLSearchParams(window.location.search)
-    const list_block = input.closest('.list')
     const list_id = list_block.getAttribute('id')
-
-    const list_items = params.get(`${list_id}items`)
-    const list_items_array = list_items ? list_items.split(',') : []
-
-    list_items_array.push(input.value)
-    params.set(`${list_id}items`, list_items_array)
-
-    // Recreate the checks parameter value based on the actual checkbox values
-    const checks = Array.from(list_block.querySelectorAll('input[type=checkbox]'))
-        .map(checkbox => checkbox.checked ? '1' : '0')
-        .join('')
-
-    params.set(`${list_id}checks`, checks + '0')
-
     const tc = document.querySelector('.list-template').content
-    const new_item = tc.querySelector('.list li').cloneNode(true)
-    const new_item_number = list_items_array.length - 1
-    const new_item_id = `${list_id}-${new_item_number}`
 
-    const new_item_input = new_item.querySelector('input')
-    new_item_input.setAttribute('value', input.value)
-    new_item_input.setAttribute('name', new_item_id)
-    new_item_input.setAttribute('id', new_item_id)
-    new_item_input.removeAttribute('checked')
-
-    // Since we _deep_ cloned the template list item, we replace the `innerText`
-    // rather than just appending `document.createTextNode`
-    const new_item_label = new_item.querySelector('label')
-    new_item_label.setAttribute('for', new_item_id)
-    new_item_label.innerText = input.value
-
-    // Identify an existing list or create a new one
+    // Use an existing list or create a new one
     const list_list = (function() {
         const existing_list_list = list_block.querySelector('ul, ol')
-        if (existing_list_list === null) {
-            const new_list_list = tc.querySelector('ul').cloneNode()
-            list_block.appendChild(new_list_list)
 
-            // Add the list type to the query parameters
-            params.set(`${list_id}type`, 'ul')
+        if (existing_list_list !== null) return existing_list_list
 
-            /* Empty list blocks have one _Add Item_ input, but list blocks with
-             * items have _Add Item_ fields before and after the list of items.
-            */
-            list_block.appendChild(tc.querySelector('.list__add-item').cloneNode(true))
+        const list_list = tc.querySelector('ul').cloneNode()
+        list_block.appendChild(list_list)
 
-            return new_list_list
-        } else {
-            return existing_list_list
-        }
+        addNewItemField(list_block)
+
+        return list_list
     })()
 
-    list_list.appendChild(new_item)
+    const items = Array.from(list_list.querySelectorAll('li'))
 
-    // The toString method of URLSearchParams encodes commas to '%2C', but we
-    // want the URL to remain user-friendly and hackable, so we change it back
-    // to commas
-    history.replaceState(null, '', '?' + params.toString().replace(/%2C/g, ','))
+    // If the list didn't have any items yet, add the "No items." message.
+    if (items.length === 0) {
+        list_block.querySelector('.no-items').remove()
+    }
 
-    input.value = ''
+    // Add the items
+    values.split(',').forEach((value, index) => {
+        const decoded_value = decodeURIComponent(value)
+        // Create the new item.
+        const item = tc.querySelector('li').cloneNode(true)
+        const item_id_number = items.length + index + 1
+        const item_id = list_id + '-' + item_id_number
+
+        // Set up the checkbox.
+        const checkbox = item.querySelector('input[type=checkbox]')
+        checkbox.setAttribute('value', value)
+        checkbox.setAttribute('name', item_id)
+        checkbox.setAttribute('id', item_id)
+
+        const check = checks.substring(index, index + 1)
+
+        if (check === '1') {
+            checkbox.setAttribute('checked', 'checked')
+        } else {
+            checkbox.removeAttribute('checked')
+        }
+
+        //checkbox.removeAttribute('checked')
+        checkbox.addEventListener('change', updateCheck)
+
+        // Set up the label.
+        /* Since we _deep_ cloned the template list item, we replace the
+         * `innerText` rather than just appending `document.createTextNode`.
+         */
+        const label = item.querySelector('label')
+        label.setAttribute('for', item_id)
+        label.innerText = decoded_value
+
+        // Add the new item to the page.
+        list_list.appendChild(item)
+
+        // Add the new item to the list of items.
+        items.push(item)
+    })
+
+    // Use the list of items to update the query parameter values.
+    const param = {
+        items: [],
+        checks: []
+    }
+
+    items.forEach((item) => {
+        const checkbox = item.querySelector('input[type=checkbox]')
+        param.items.push(checkbox.value)
+        param.checks.push(checkbox.checked ? '1' : '0')
+    })
+
+    params.set(list_id + 'items', param.items.join(','))
+    params.set(list_id + 'checks', param.checks.join(''))
+
+    setUrl(params)
 }
 
-function addList(event) {
-    const input = event.target
-    const lists_container = input.closest('.lists')
+function addItemHandler(event) {
+    const values = encodeURIComponent(event.target.value)
+    const checks = ''
+    const list_block = event.target.closest('.list')
+    addItems({values, checks, list_block})
+    event.target.value = ''
+}
 
+function addList({container, heading}) {
     const tc = document.querySelector('.list-template').content
-    const new_list = tc.querySelector('.list').cloneNode()
+    const list_block = tc.querySelector('.list').cloneNode()
 
-    /* Iterate over existing list ids and incrementing the greatest of them by 1.
-     * This accommodates missing list id numbers as well as disordered list id numbers.
+    const lists = Array.from(container.querySelectorAll('.list'))
+    const add_list_blocks = container.querySelectorAll('.add-list')
+    const no_lists_message = container.querySelector('.no-lists') ?? null
+
+    /* Generate a new list id number by incrementing the highest existing list
+     * id number by 1.
+     * This accommodates missing list id numbers as well as disordered list id
+     * numbers.
      */
-    const lists = Array.from(lists_container.querySelectorAll('.list'))
-    const list_id_number = lists.reduce((id_number, current_list) => {
-        const current_id = current_list.getAttribute('id')
-        const current_id_value = +current_id.substring(1)
+    const id_number = lists.reduce((id_number, list) => {
+        const current_id = list.getAttribute('id')
+        const current_id_number = +current_id.substring(1)
 
-        if (current_id_value >= id_number) {
-            return current_id_value + 1
+        if (current_id_number >= id_number) {
+            return current_id_number + 1
         } else {
-            return id
+            return id_number
         }
     }, 1)
 
-    const list_id = 'l' + list_id_number
-    new_list.setAttribute('id', list_id)
+    const list_id = 'l' + id_number
+    list_block.setAttribute('id', list_id)
 
     const list_heading = tc.querySelector('.list__heading').cloneNode()
-    list_heading.appendChild(document.createTextNode(input.value))
-    new_list.appendChild(list_heading)
+    list_heading.appendChild(document.createTextNode(heading))
+    list_block.appendChild(list_heading)
+
+    // Add the new list block after the last list block or, if this is the first
+    // list, after _New List_ field.
+    if (lists.length > 0) {
+        lists.toReversed()[0].insertAdjacentElement('afterend', list_block)
+    } else if (add_list_blocks.length > 0) {
+        add_list_blocks[0].insertAdjacentElement('afterend', list_block)
+    } else {
+        container.appendChild(list_block)
+    }
+
+    // If this was the first list, add a _New List_ field at the end.
+    if (lists.length === 0) {
+        addNewListField(container)
+    }
+
+    if (no_lists_message !== null) no_lists_message.remove()
+
+    // Add a "No items." message for the new list.
+    addNoItemsMessage(list_block)
 
     // TODO: Add a list type selector (ordered / unordered)
 
-    const add_item = tc.querySelector('.list__add-item').cloneNode(true)
-    add_item.addEventListener('change', addItem)
-    new_list.appendChild(add_item)
-
-    // Insert the new list before the last _Add List_ block
-    const add_list_blocks = Array.from(lists_container.querySelectorAll('.add-list'))
-    const last_add_list_block = add_list_blocks.toReversed()[0]
-    lists_container.insertBefore(new_list, last_add_list_block)
+    // Add an _Add Item_ field for the new list.
+    const new_item_field = addNewItemField(list_block)
 
     const params = new URLSearchParams(window.location.search)
-    params.set(list_id + 'name', input.value)
+    params.set(list_id + 'name', heading)
 
-    // The toString method of URLSearchParams encodes commas to '%2C', but we
-    // want the URL to remain user-friendly and hackable, so we change it back
-    // to commas
-    history.replaceState(null, '', '?' + params.toString().replace(/%2C/g, ','))
-
-    input.value = ''
-
-    location.replace('#' + list_id)
+    setUrl(params)
+    return list_block
 }
 
-function getLists(params) {
+function addListHandler(event) {
+    const input = event.target
+    const container = input.closest('.lists')
+    const heading = input.value
+    const list_block = addList({container, heading})
+    input.value = ''
+
+    // TODO: Add `scroll-behavior: smooth` to CSS for this:
+    list_block.scrollIntoView({behavior: 'smooth'})
+    /* Focus the empty list's _Add Item_ field after the UI has had a chance to
+     * smoothly scroll into view.
+     * The `preventScroll` option should make this unnecessary, but it isn't
+     * currently supported on mobile browsers.
+     */
+    setTimeout(() => {
+        const add_item_field = list_block.querySelector('.list__add-item input')
+        add_item_field.focus({preventScroll: true})
+    }, 500)
+}
+
+function getLists() {
+    const params = new URLSearchParams(window.location.search)
     return Array.from(params.keys())
         .map((param) => {
             const list_name_match = param.match(/l(\d+)name/)
@@ -225,4 +230,63 @@ function getLists(params) {
             }
         })
         .filter((list) => list !== null)
+}
+
+function updateCheck(event) {
+    const checkbox = event.target
+    const list = checkbox.closest('.list')
+    const list_id = list.getAttribute('id')
+    const list_checkboxes = list.querySelectorAll('input[type=checkbox]')
+
+    const params = new URLSearchParams(window.location.search)
+    const checks = params.get((list_id + 'checks'))
+
+    const check_values = (function() {
+        if (checks === null) {
+            return list_checkboxes.map(checkbox => checkbox.checked ? '1' : '0')
+        } else {
+            return checks.split('')
+        }
+    })()
+
+    /* Checkbox id numbers are natural counting numbers for user-friendly and
+     * hackable URLs.
+     * To get the array index we subtract 1 from the checkbox id number
+     */
+    const checkbox_number = checkbox.getAttribute('id').match(/-(\d+)$/)[1]
+    const check_values_index = checkbox_number - 1
+
+    check_values[check_values_index] = checkbox.checked ? '1' : '0'
+    params.set(`${list_id}checks`, check_values.join(''))
+    setUrl(params)
+}
+
+function setUrl(params) {
+    // The toString method of URLSearchParams encodes commas to '%2C', but we
+    // want the URL to remain user-friendly and hackable, so we change it back
+    // to commas
+    history.replaceState(null, '', '?' + params.toString().replace(/%2C/g, ','))
+}
+
+function addNewItemField(list_block) {
+    const tc = document.querySelector('.list-template').content
+    const add_item = tc.querySelector('.list__add-item').cloneNode(true)
+    add_item.addEventListener('change', addItemHandler)
+    list_block.appendChild(add_item)
+    return add_item
+}
+
+function addNewListField(lists_container) {
+    const tc = document.querySelector('.list-template').content
+    const add_list = tc.querySelector('.add-list').cloneNode(true)
+    add_list.addEventListener('change', addListHandler)
+    lists_container.appendChild(add_list)
+    return add_list
+}
+
+function addNoItemsMessage(list_block) {
+    const no_items = document.createElement('p')
+    no_items.classList.add('no-items')
+    no_items.appendChild( document.createTextNode('No items.') )
+    list_block.appendChild(no_items)
 }
